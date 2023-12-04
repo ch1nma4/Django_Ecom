5c7cc008f1975f08ed0e29fec25792fd0f0bb1a1
Chinmay <chinmaymayekar06@gmail.com>
2023-12-04 20:40:34 +0530

    commit:5

--- mysite/Ecommerce/urls.py
+++ mysite/Ecommerce/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path 
-from Ecommerce import views
+from . import views
 
-app_name = 'Ecommerce'
 
 urlpatterns = [
 
-    path('store/' , views.store , name='store'),
+    path('' , views.store , name='store'),
     path('cart/' , views.cart , name='cart'),
     path('checkout/' , views.checkout , name='checkout'),
     
